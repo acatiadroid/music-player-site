56cea6887592b3e0440ca77f855aeac123f54f2d
cosrnic <75631523+cosrnic@users.noreply.github.com>
2023-12-04 16:18:44 +0000

    Fix a missspell

--- src/components/Roadmap.tsx
+++ src/components/Roadmap.tsx
@@ -1,21 +1,21 @@
 export const Roadmap = () => (
-    <div className="bg-dark rounded-xl lg:w-1/2 w-100 mx-auto h-96 p-2 text-center text-xl leading-10">
+    <div className="p-2 mx-auto text-xl leading-10 text-center bg-dark rounded-xl lg:w-1/2 w-100 h-96">
         <br />
         <p className="text-2xl">Planned Updates:</p>
         <br />
-        <div className="justify-center flex">
+        <div className="flex justify-center">
             <p>Queuing System</p>
-            <div className="w-16 h-4 bg-main rounded-full my-auto mx-2"></div>
+            <div className="w-16 h-4 mx-2 my-auto rounded-full bg-main"></div>
             <p>Coming Soon</p>
         </div>
-        <div className="justify-center flex">
-            <p>Audio Straming</p>
-            <div className="w-16 h-4 bg-main rounded-full my-auto mx-2"></div>
+        <div className="flex justify-center">
+            <p>Audio Streaming</p>
+            <div className="w-16 h-4 mx-2 my-auto rounded-full bg-main"></div>
             <p>Coming Soon</p>
         </div>
-        <div className="justify-center flex">
+        <div className="flex justify-center">
             <p>Playlists</p>
-            <div className="w-16 h-4 bg-main rounded-full my-auto mx-2"></div>
+            <div className="w-16 h-4 mx-2 my-auto rounded-full bg-main"></div>
             <p>Coming Soon</p>
         </div>
     </div>
